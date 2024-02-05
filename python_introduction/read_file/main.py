da423f1a26bbba739e967600064259b382ddf615
ValicioKely <razalicio008@gmail.com>
2024-02-05 21:21:58 +0300

    Read file

--- python_introduction/read_file/main.py
+++ python_introduction/read_file/main.py
@@ -1,8 +1,7 @@
-data_from_dict = 'read_file/data.txt'
+data_from_dict = 'data.txt'
 
 with open(data_from_dict, 'r') as source_file:
     data = source_file.readline()
-    print(data)
     for i in range(0, len(data)):
         data[i] = data[i].rstrip()
         print(data)
